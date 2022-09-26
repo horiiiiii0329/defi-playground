c9c7b82b81f48bdc5b5e0e2e891e4d55309fe106
Hikaru HOrinouchi <hori0329@gmail.com>
2022-09-26 22:28:15 +0900

    first commit

---
+++ scripts/aaveBorrow.js
@@ -0,0 +1,50 @@
+const { getNamedAccounts, ethers } = require("hardhat");
+const { getWeth, AMOUNT } = require("../scripts/getWeth");
+
+async function main() {
+  await getWeth();
+
+  const { deployer } = await getNamedAccounts();
+  const lendingPool = await getLendingPool(deployer);
+  console.log(`LendingPool address ${lendingPool.address}`);
+
+  //deposit!
+  const wethTokenADdress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  await approveErc20(wethTokenADdress, lendingPool.address, AMOUNT, deployer);
+  await lendingPool.deposit(wethTokenADdress, AMOUNT, deployer, 0);
+  console.log("Deposited");
+}
+
+async function approveErc20(
+  erc20Address,
+  spenderAddress,
+  amountToSpend,
+  account
+) {
+  const erc20Token = await ethers.getContractAt(
+    "IERC20",
+    erc20Address,
+    account
+  );
+  const tx = await erc20Token.approve(spenderAddress, amountToSpend);
+  await tx.wait(1);
+  console.log("Approved");
+}
+
+async function getLendingPool(account) {
+  const lendingPoolAddressesProvider = await ethers.getContractAt(
+    "ILendingPoolAddressesProvider",
+    "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+    account
+  );
+  const lendingPoolAddress =
+    await lendingPoolAddressesProvider.getLendingPool();
+  const lendingPool = await ethers.getContractAt(
+    "ILendingPool",
+    lendingPoolAddress,
+    account
+  );
+  return lendingPool;
+}
+
+main();
